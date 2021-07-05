136c1ba8b2433c0dbb0a8ac5a8842410cf268e4b
rucpata <rucinska.patrycja@gmail.com>
2021-07-05 15:04:04 +0200

    site metadata & page queries

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata: {
+    title: 'Web Worrior',
+    description: 'web dev portfolio',
+    copyright: 'This website is scopyright 2021 Warrior'
+  }
 }
