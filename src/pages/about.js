315cc8c940aaef0ecc98af4b9cfb3a3303c04ee1
rucpata <rucinska.patrycja@gmail.com>
2021-07-05 13:38:34 +0200

    layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 export default function About() {
     return (
-        <div>
-            <h1>About Page</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur accusantium quod culpa blanditiis repellat velit ducimus repellendus dolores non dolor beatae harum quibusdam a dolorum animi ullam et, assumenda consequatur?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, commodi assumenda. Libero deleniti soluta iure magni asperiores doloribus nihil, nostrum id commodi est ea eveniet velit accusamus sequi tenetur blanditiis.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At incidunt ab delectus natus dolorem nesciunt doloribus, quia reprehenderit eum officiis fugit cupiditate explicabo dignissimos ut est, autem commodi aliquam aliquid!</p>
-        </div>
+        <Layout>
+            <div>
+                <h1>About Page</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur accusantium quod culpa blanditiis repellat velit ducimus repellendus dolores non dolor beatae harum quibusdam a dolorum animi ullam et, assumenda consequatur?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, commodi assumenda. Libero deleniti soluta iure magni asperiores doloribus nihil, nostrum id commodi est ea eveniet velit accusamus sequi tenetur blanditiis.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At incidunt ab delectus natus dolorem nesciunt doloribus, quia reprehenderit eum officiis fugit cupiditate explicabo dignissimos ut est, autem commodi aliquam aliquid!</p>
+            </div>
+            
+        </Layout>
     )
 }
