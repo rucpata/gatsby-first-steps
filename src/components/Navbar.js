9876e99aef2a2298857f2e60b6485d676df76266
rucpata <rucinska.patrycja@gmail.com>
2021-07-05 15:10:30 +0200

    useStaticQuery

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,22 @@
-import { Link } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
 
 export default function Navbar() {
+    const data = useStaticQuery(graphql`
+        query SiteInfo {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
+        }
+    `)
+
+    const {title} = data.site.siteMetadata;
+
     return (
         <nav>
-            <h1>Web Worrior</h1>
+            <h1>{title}</h1>
             <div className="links">
                 <Link to='/'>Home</Link>
                 <Link to='/about'>About</Link>
@@ -13,3 +25,6 @@ export default function Navbar() {
         </nav>
     )
 }
+
+
+
