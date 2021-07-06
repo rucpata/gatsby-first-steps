5bd507f7b13a5f2c781a89804f449f799db05127
rucpata <rucinska.patrycja@gmail.com>
2021-07-06 17:20:47 +0200

    query variables

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -7,7 +7,7 @@ export default function ProjectDetails() {
     return (
         <Layout>
             <div className={styles.details}>
-                <h2>title</h2>
+                <h2>{title}</h2>
                 <h3>stack</h3>
                 <div className={styles.featured}>
                     {/* <Img fluid={}/> */}
